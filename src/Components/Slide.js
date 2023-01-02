67519f0985b3da5b98e72d279afeab0b5332db8e
Sakib Biswas <sakibbiswas418@gmail.com>
2023-01-02 18:34:27 +0530

    slider done

--- src/Components/Slide.js
+++ src/Components/Slide.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Slide = ({ slide }) => {
   return (
@@ -7,7 +8,19 @@ const Slide = ({ slide }) => {
       style={{
         backgroundImage: `url(${slide.src})`,
       }}
-    ></div>
+    >
+      <div className="slide-text container mx-auto text-violet-50 flex flex-col items-start justify-center h-full gap-5">
+        <h1 className="text-7xl w-4/5 font-bold uppercase space-font">
+          {slide.headline}
+        </h1>
+        <p className="w-3/5">{slide.body}</p>
+        <Link className="slide-btn h-14 w-72  py-3 px-4 border border-violet-50 hover:text-orange-50 duration-300 relative bg-transparent uppercase font-medium mt-5">
+          <span className="z-[2] absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-full text-center">
+            {slide.cta}
+          </span>
+        </Link>
+      </div>
+    </div>
   );
 };
 
--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -1,55 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import Slide from "./Slide";
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
+
+const data = [
+  {
+    id: 1,
+    src: "https://i.ibb.co/XszmG02/camera.jpg",
+    headline: "DSLR cameras for stunning photos",
+    body: "Are you an aspiring photographer looking to take your skills to the next level? Our DSLR cameras offer advanced features and high-quality image sensors to help you capture stunning photos. From landscape shots to portraits, these cameras are perfect for capturing all types of subjects.",
+    cta: "Shop DSLR cameras now",
+    category: "cameras",
+  },
+  {
+    id: 2,
+    src: "https://i.ibb.co/mtc8v16/tv.jpg",
+    headline: "Upgrade your home entertainment with our TVs",
+    body: "Experience the latest in home entertainment with our selection of TVs. From sleek and modern designs to advanced features like 4K resolution and smart capabilities, our TVs will bring your favorite movies, TV shows, and streaming content to life.",
+    cta: "Shop TVs and upgrade now",
+    category: "tvs",
+  },
+  {
+    id: 3,
+    src: "https://i.ibb.co/kmr5qQv/headphones.jpg",
+    headline: "Enhance your listening experience",
+    body: "Take your music, movies, and more to the next level with our headphones. Our selection offers a range of styles and features, including noise-cancelling technology, wireless connectivity, and comfortable designs for all-day wear.",
+    cta: "Experience enhanced sound",
+    category: "headphones",
+  },
+  {
+    id: 4,
+    src: "https://i.ibb.co/JqxDhvZ/console.jpg",
+    headline: "Take your gaming to the next level",
+    body: "Elevate your gaming experience with our selection of gaming consoles. From the latest models to classic systems, we have a console for every type of gamer. Our consoles offer advanced graphics, fast processing speeds, and a variety of exclusive games to choose from.",
+    cta: "Shop consoles and play now",
+    category: "consoles",
+  },
+  {
+    id: 5,
+    src: "https://i.ibb.co/YbS7mL2/smart-watches.jpg",
+    headline: "Stay connected with smart watches",
+    body: "Stay connected and on top of your day with our smart watches. Our selection offers a range of styles and features, including fitness tracking, phone notifications, and voice assistants. These watches are the perfect combination of functionality and style.",
+    cta: "Connect with a smart watch",
+    category: "smart-watches",
+  },
+];
 
 const Slider = () => {
-  const data = [
-    {
-      id: 1,
-      src: "https://i.ibb.co/XszmG02/camera.jpg",
-      headline: "DSLR cameras for stunning photos",
-      body: "Are you an aspiring photographer looking to take your skills to the next level? Our DSLR cameras offer advanced features and high-quality image sensors to help you capture stunning photos. From landscape shots to portraits, these cameras are perfect for capturing all types of subjects.",
-      cta: "Shop DSLR cameras now",
-      category: "cameras",
-    },
-    {
-      id: 2,
-      src: "https://i.ibb.co/mtc8v16/tv.jpg",
-      headline: "Upgrade your home entertainment with our TVs",
-      body: "Experience the latest in home entertainment with our selection of TVs. From sleek and modern designs to advanced features like 4K resolution and smart capabilities, our TVs will bring your favorite movies, TV shows, and streaming content to life.",
-      cta: "Shop TVs and upgrade now",
-      category: "tvs",
-    },
-    {
-      id: 3,
-      src: "https://i.ibb.co/kmr5qQv/headphones.jpg",
-      headline: "Enhance your listening experience",
-      body: "Take your music, movies, and more to the next level with our headphones. Our selection offers a range of styles and features, including noise-cancelling technology, wireless connectivity, and comfortable designs for all-day wear.",
-      cta: "Experience enhanced sound",
-      category: "headphones",
-    },
-    {
-      id: 4,
-      src: "https://i.ibb.co/JqxDhvZ/console.jpg",
-      headline: "Take your gaming to the next level",
-      body: "Elevate your gaming experience with our selection of gaming consoles. From the latest models to classic systems, we have a console for every type of gamer. Our consoles offer advanced graphics, fast processing speeds, and a variety of exclusive games to choose from.",
-      cta: "Shop consoles and play now",
-      category: "consoles",
-    },
-    {
-      id: 5,
-      src: "https://i.ibb.co/YbS7mL2/smart-watches.jpg",
-      headline: "Stay connected with smart watches",
-      body: "Stay connected and on top of your day with our smart watches. Our selection offers a range of styles and features, including fitness tracking, phone notifications, and voice assistants. These watches are the perfect combination of functionality and style.",
-      cta: "Connect with a smart watch",
-      category: "smart-watches",
-    },
-  ];
+  const [currrentSlide, setCurrentSlide] = useState(0);
+
+  const prevSlide = () => {
+    setCurrentSlide(
+      currrentSlide === 0 ? data.length - 1 : (prevSlide) => prevSlide - 1
+    );
+  };
+  const nextSlide = () => {
+    setCurrentSlide(
+      currrentSlide === data.length - 1 ? 0 : (prevSlide) => prevSlide + 1
+    );
+  };
 
   return (
-    <div className="slider w-[500vw] h-[calc(100vh-80px)] flex -translate-x-[100vw]">
-      {data.map((slide) => (
-        <Slide slide={slide} key={slide.id} />
-      ))}
+    <div className="frame relative">
+      <div
+        className="slider w-[500vw] h-[calc(100vh-80px)] flex duration-1000"
+        style={{ transform: `translateX(-${100 * currrentSlide}vw)` }}
+      >
+        {data.map((slide) => (
+          <Slide slide={slide} key={slide.id} />
+        ))}
+      </div>
+      <div className="btns w-fit m-auto absolute z-[1] left-0 right-0 bottom-20 text-white flex gap-10 justify-center text-2xl">
+        <button
+          className="prev-btn h-14 w-20 bg-gray-700/50 shadow-sm  shadow-black/30 backdrop-blur-sm hover:bg-gray-800/50 flex items-center justify-center hover:text-black/60 duration-300 border border-gray-500"
+          onClick={prevSlide}
+        >
+          <BsArrowLeft />
+        </button>
+        <button
+          className="next-btn h-14 w-20 p-3 bg-gray-700/50 shadow-sm backdrop-blur-sm hover:bg-gray-800/50 flex items-center justify-center hover:text-black/60 duration-300 border border-gray-500"
+          onClick={nextSlide}
+        >
+          <BsArrowRight />
+        </button>
+      </div>
     </div>
   );
 };
